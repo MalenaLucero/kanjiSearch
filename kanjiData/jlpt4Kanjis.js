ae58f7eda0d708142198f510eadeae0620474d62
MalenaLucero <luceromalena@hotmail.com>
2021-07-18 12:52:02 -0300

    search only not repeated kanji

---
+++ kanjiData/jlpt4Kanjis.js
@@ -0,0 +1,7 @@
+const jlpt4Kanjis = [
+    '上',
+    '大',
+    '出',
+]
+
+module.exports = jlpt4Kanjis
--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 const fetch = require('node-fetch')
 const fs = require('fs')
 
-const expressions = require('./260621expressions')
-const kanjis260621 = require('./260621kanjis.js')
+const jlpt4Kanjis = require('./kanjiData/jlpt4Kanjis')
+const notJlpt4Kanjis = require('./kanjiData/notJlpt4Kanjis')
+const uploadedKanji = notJlpt4Kanjis.concat(jlpt4Kanjis)
+
+const expressions = require('./100721expressions')
 
 const kanjiArray = expressions.map(e => e.word).join('').split('')
     .filter(char => char.charCodeAt() >= 13312 && char.charCodeAt() < 65306)
 const kanjiSet = new Set(kanjiArray)
+const notUploadedKanji = Array.from(kanjiSet).filter(kanji => {
+    if (!uploadedKanji.includes(kanji)) {
+        return kanji
+    }
+})
 
-const populateTxtFiles = kanjiSet => {
-    kanjiSet.forEach(kanji => {
+const populateTxtFiles = kanjiToSearch => {
+    kanjiToSearch.forEach(kanji => {
         const URI = 'https://kanjiapi.dev/v1/kanji/' + kanji
         const encodedURI = encodeURI(URI)
         
@@ -17,13 +25,15 @@ const populateTxtFiles = kanjiSet => {
             .then(res => res.json())
             .then(json => {
                 if (json.jlpt === 4) {
-                    fs.appendFile('./jlpt4.txt', json.kanji, () => {})
+                    fs.appendFile('./txtFiles/jlpt4.txt', `'${json.kanji}',\r\n`, () => {})
                 } else {
-                    fs.appendFile('./kanjiData.txt', JSON.stringify(json) + ",\r\n", () => {})
+                    fs.appendFile('./txtFiles/kanjiData.txt', `${JSON.stringify(json)},\r\n`, () => {})
+                    fs.appendFile('./txtFiles/notJlpt4Kanjis.txt', `'${json.kanji}',\r\n`, () => {})
                 }
             })
             .catch(error => console.log(error))
     })
 }
 
+//populateTxtFiles(notUploadedKanji)
 
